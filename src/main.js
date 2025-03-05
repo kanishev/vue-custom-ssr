e7141107f8acbd22ad5c44fd4e30500b7d54f983
Nick <50417170+kanishev@users.noreply.github.com>
2025-03-05 22:54:30 +0300

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createSSRApp } from "vue";
 import { createAppRouter } from "./router";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
 export function createApp() {
     const app = createSSRApp(App);
     const router = createAppRouter();
+    const pinia = createPinia();
 
+    app.use(pinia);
     app.use(router);
+
     return { app, router };
 }

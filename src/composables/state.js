4cfd880236ba28c950ac6d276143529de59f7664
Nick <50417170+kanishev@users.noreply.github.com>
2025-04-04 20:18:49 +0300

    fix useState ref to toRef

--- src/composables/state.js
+++ src/composables/state.js
@@ -1,9 +1,9 @@
-import { ref } from "vue";
+import { ref, toRef } from "vue";
 import { getAppInstance } from "../utils/appInstance";
 
 export function useState(key, init = null) {
     const instance = getAppInstance();
-    const state = ref(instance.config.globalState[key]);
+    const state = toRef(instance.config.globalState, key);
 
     if (state.value === undefined && init) {
         const initialValue = init();
